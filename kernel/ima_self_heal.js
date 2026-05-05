a700aa0be900d86fa15e016f39ac3d51b35a1209
Ori Cohen <imaosglobal@gmail.com/>
2026-05-06 00:00:35 +0300

    IMA HARDENED SYSTEM

--- kernel/ima_self_heal.js
+++ kernel/ima_self_heal.js
@@ -1,19 +1,16 @@
 
 const fs = require('fs');
 
-module.exports = {
-  analyze(){
-    const issues = [];
-    const files = fs.readdirSync('.');
-    if(!files.includes('ima_kernel.js')) issues.push('missing kernel');
-    return issues;
-  },
+function heal(){
+  const required = ['ima_kernel.js','ima_runtime.js','ima_policy.js'];
 
-  fix(issues){
-    if(issues.length){
-      console.log('[HEAL] fixing:', issues);
-    } else {
-      console.log('[HEAL] system clean');
+  required.forEach(f=>{
+    if(!fs.existsSync(f)){
+      console.log('[HEAL] missing',f);
     }
-  }
-};
+  });
+
+  console.log('[HEAL CHECK DONE]');
+}
+
+module.exports = { heal };
