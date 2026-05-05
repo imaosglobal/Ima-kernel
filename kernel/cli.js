74433c752c7d30306fc03576d561c64cadb36b0d
Ori Cohen <imaosglobal@gmail.com/>
2026-05-06 00:34:41 +0300

    release 1.0.1778016879892

--- kernel/cli.js
+++ kernel/cli.js
@@ -1,5 +1,10 @@
-
 #!/usr/bin/env node
-const cp = require('child_process');
+const http = require('http');
 const cmd = process.argv[2] || 'run';
-cp.execSync('curl -s http://localhost:4000/' + cmd,{stdio:'inherit'});
+const host = process.env.IMA_HOST || 'localhost';
+
+http.get('http://' + host + ':4000/' + cmd, res => {
+  res.pipe(process.stdout);
+}).on('error', () => {
+  console.log('server not reachable');
+});
