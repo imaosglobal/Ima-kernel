a7b5a4313ce960cb458c4273e154f642a488bb86
Ori Cohen <imaosglobal@gmail.com/>
2026-05-11 15:47:17 +0300

    IMA AUTO 1.0.1778503633574

--- kernel/cli.js
+++ kernel/cli.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
 
-const http=require('http');
-
-const cmd=process.argv[2]||'status';
-
-const host=process.env.IMA_HOST||'localhost';
-
-const port=process.env.IMA_PORT||4000;
-
-http.get(
-'http://'+host+':'+port+'/'+cmd,
-res=>res.pipe(process.stdout)
-).on(
-'error',
-()=>{
-console.log('IMA OFFLINE');
-}
-);
+console.log('IMA CLI ONLINE');
